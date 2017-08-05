a5a3ec17493a6be7493dc4b0f21565c345eafcac
Carles Fernandez <carles.fernandez@gmail.com>
2017-08-05 19:46:58 +0200

    Fix javascript in sidebar

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,12 +4,30 @@
 
    $(document).ready(function(){
 
+       // Sticky footer
+       var bumpIt = function() {
+           $('body').css('margin-bottom', $('.page__footer').outerHeight(true));
+         },
+         didResize = false;
+
+       bumpIt();
+
+       $(window).resize(function() {
+         didResize = true;
+       });
+       setInterval(function() {
+         if(didResize) {
+           didResize = false;
+           bumpIt();
+         }
+       }, 250);
+       
      // FitVids init
      $("#main").fitVids();
 
      // Follow menu drop down
      $(".author__urls-wrapper button").on("click", function() {
-       $(".author__urls").fadeToggle("fast", function() {});
+       $(".author__urls").toggleClass("is--visible");
        $(".author__urls-wrapper button").toggleClass("open");
      });
 
