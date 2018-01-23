dedc2fce759b570458b782630403dd2eb5c65530
Carles Fernandez <carlesfernandez@gmail.com>
2018-01-23 16:53:06 +0100

    Improve sidebar

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -21,9 +21,30 @@
            bumpIt();
          }
        }, 250);
-       
+
      // FitVids init
      $("#main").fitVids();
+
+     // Sticky sidebar
+var stickySideBar = function() {
+  var show =
+    $(".author__urls-wrapper button").length === 0
+      ? $(window).width() > 1024 // width should match $large Sass variable
+      : !$(".author__urls-wrapper button").is(":visible");
+  if (show) {
+    // fix
+    $(".sidebar").addClass("sticky");
+  } else {
+    // unfix
+    $(".sidebar").removeClass("sticky");
+  }
+};
+
+stickySideBar();
+
+$(window).resize(function() {
+  stickySideBar();
+});
 
      // Follow menu drop down
      $(".author__urls-wrapper button").on("click", function() {
