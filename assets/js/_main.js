d0aa77f6b1dd82e4b7b590ad7d4477541c275a8a
Carles Fernandez <carles.fernandez@gmail.com>
2019-04-12 08:37:52 +0200

    Fix Javascript console error at the bottom of pages without TOC

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -69,26 +69,29 @@ $(document).ready(function() {
   var scroll = new SmoothScroll('a[href*="#"]', {
     offset: 20,
     speed: 300,
-    durationMax: 600
+    durationMax: 600,
+    easing: 'easeInOutQuint'
   });
 
   // Gumshoe scroll spy init
-  var spy = new Gumshoe("nav.toc a", {
-    // Active classes
-    navClass: "active", // applied to the nav list item
-    contentClass: "active", // applied to the content
+  if($("nav.toc").length > 0) {
+    var spy = new Gumshoe("nav.toc a", {
+      // Active classes
+      navClass: "active", // applied to the nav list item
+      contentClass: "active", // applied to the content
 
-    // Nested navigation
-    nested: false, // if true, add classes to parents of active link
-    nestedClass: "active", // applied to the parent items
+      // Nested navigation
+      nested: false, // if true, add classes to parents of active link
+      nestedClass: "active", // applied to the parent items
 
-    // Offset & reflow
-    offset: 20, // how far from the top of the page to activate a content area
-    reflow: true, // if true, listen for reflows
+      // Offset & reflow
+      offset: 20, // how far from the top of the page to activate a content area
+      reflow: true, // if true, listen for reflows
 
-    // Event support
-    events: true // if true, emit custom events
-  });
+      // Event support
+      events: true // if true, emit custom events
+    });
+  }
 
   // add lightbox class to all image links
   $(
